e9e7a43124cec31eec4d86640b35899328af9fe2
D.Oliynuk <dima-oleynik@i.ua>
2025-11-20 05:34:47 +0200

    Add project files.

---
+++ 30.10.25 Dynamyc_Struct/Queue.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+template<typename T>
+class Queue
+{
+	struct Node
+	{
+		T el;
+		Node* next;
+	};
+	Node* front;
+	Node* back;
+public:
+	Queue()
+	{
+		front = nullptr;
+		back = nullptr;
+	}
+	~Queue();
+	void enqueue(const T&);
+	T dequeue();
+	bool isEmpty();
+};
+
+template<typename T>
+inline Queue<T>::~Queue()
+{
+	Node* nodePtr = front;
+	Node* nextNode = nullptr;;
+	while (nodePtr != nullptr)
+	{
+		nextNode = nodePtr->next;
+		delete nodePtr;
+		nodePtr = nextNode;
+	}
+}
+
+template<typename T>
+inline void Queue<T>::enqueue(const T& inf)
+{
+	Node* newNode = new Node;
+	newNode->el = inf;
+	newNode->next = nullptr;
+	if (front == nullptr)
+	{
+		front = back = newNode;
+	}
+	else
+	{
+		back->next = newNode;
+		back = newNode;
+	}
+}
+
+
+template<typename T>
+inline T Queue<T>::dequeue()
+{
+	Node* tmp;
+	if (!isEmpty())
+	{
+		T val = front->el;
+		tmp = front->next;
+		delete front;
+		front = tmp;
+		if (front == nullptr)
+			back = nullptr;
+		return val;
+	}
+
+}
+
+template<typename T>
+inline bool Queue<T>::isEmpty()
+{
+	if (front == nullptr)
+		return true;
+	return false;
+}
---
+++ 30.10.25 Dynamyc_Struct/Source.cpp
@@ -0,0 +1,62 @@
+﻿//Динамічні структури даних – стек
+//Завдання
+//Є рядок символів, ознакою кінця якого є; .У рядку можуть бути фігурні, круглі, квадратні дужки.Дужки можуть бути відкриваючими та закриваючими.
+//Необхідно перевірити коректність розміщення дужок.При цьому необхідно, щоб виконалися такі правила :
+//1.	Кожна дужка, що відкриває, повинна мати праворуч таку саму дужку, що закриває.Зворотне також має бути правильним.
+//2.	Пари дужок різних типів, що відкривають і закривають, повинні бути правильно розташовані по відношенню одна до одної.
+//Викорстати стек, який я зробив на занятті.
+#include<iostream>
+#include"Queue.h"
+using namespace std;
+int main()
+{
+	Queue<char> q;
+	char ch;
+	cout << "Enter string ending with ; : ";
+	cin.get(ch);
+	while (ch != ';')
+	{
+		q.enqueue(ch);
+		cin.get(ch);
+	}
+	bool isCorrect = true;
+	Queue<char> stack;
+	while (!q.isEmpty() && isCorrect)
+	{
+		char curr = q.dequeue();
+		if (curr == '(' || curr == '{' || curr == '[')
+		{
+			stack.enqueue(curr);
+		}
+		else if (curr == ')' || curr == '}' || curr == ']')
+		{
+			if (stack.isEmpty())
+			{
+				isCorrect = false;
+			}
+			else
+			{
+				char top = stack.dequeue();
+				if ((curr == ')' && top != '(') ||
+					(curr == '}' && top != '{') ||
+					(curr == ']' && top != '['))
+				{
+					isCorrect = false;
+				}
+			}
+		}
+	}
+	if (!stack.isEmpty())
+	{
+		isCorrect = false;
+	}
+	if (isCorrect)
+	{
+		cout << "The string is correct." << endl;
+	}
+	else
+	{
+		cout << "The string is incorrect." << endl;
+	}
+	return 0;
+}
